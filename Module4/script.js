6f3bec4332c16ec6abd71f62a0603b8175f50cb3
Istiaqu3 <istiaque.255@gmai.com>
2020-06-14 16:24:28 +0600

    updated

--- Module4/script.js
+++ Module4/script.js
@@ -1,13 +1,8 @@
 (function (window) {
-
-var names = ["Yousuf", "jamal", "Jeny", "Jessore", "Pal", "Fraun", "Larry", "Pala", "Tomarmatha", "Jim"];
-
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i in names) {
-
- 
   var firstLetter = ((names[i]).charAt(0)).toLowerCase();
-
   if (firstLetter == 'j') {
      window.byeSpeaker.speak(names[i]);
   } else {
